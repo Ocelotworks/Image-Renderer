727caca33780e27fb0a9917789002dbf7455dc0f
Peter Maguire <me@petermaguire.xyz>
2021-02-04 17:46:27 +0000

    feat: allow for non-filled rectangles

--- filter/Rectangle.go
+++ filter/Rectangle.go
@@ -9,6 +9,10 @@ type Rectangle struct{}
 func (r Rectangle) BeforeRender(ctx *gg.Context, args map[string]interface{}, frameNum int) *gg.Context {
 	ctx.SetHexColor(args["colour"].(string))
 	ctx.DrawRectangle(args["x"].(float64), args["y"].(float64), args["w"].(float64), args["h"].(float64))
-	ctx.Fill()
+	if args["fill"].(bool) {
+		ctx.Fill()
+	} else {
+		ctx.Stroke()
+	}
 	return ctx
 }
